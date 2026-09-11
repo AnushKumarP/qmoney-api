ed4833157f3e097c72bffba3991bb1715090acdf
Anush Kumar <anushkumar358@gmail.com>
2026-09-10 20:02:54 -0500

    Add src/main/java/com/anushkumar/qmoney/QMoneyApplication.java

---
+++ src/main/java/com/anushkumar/qmoney/QMoneyApplication.java
@@ -0,0 +1,11 @@
+package com.anushkumar.qmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QMoneyApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(QMoneyApplication.class, args);
+  }
+}
